26840cd1700412310e9d23df6a2ee003b176cda6
Christian Fuchs <cfuchs@patchcode.ch>
2018-01-27 21:01:27 +0100

    + developing a TreeConstructor (or Builder)

--- src/main/java/ch/patchcode/port_royale_3/routes/GreedyMinimumDistanceSpanningTree.java
+++ src/main/java/ch/patchcode/port_royale_3/routes/GreedyMinimumDistanceSpanningTree.java
@@ -29,32 +29,41 @@ public class GreedyMinimumDistanceSpanningTree {
 
         TreeSet<Edge> edges = new TreeSet<>(centralVertex.getEdges());
 
-        Map<Vertex, List<Vertex>> tree = new HashMap<>();
-        tree.put(centralVertex, new ArrayList<>());
-
-        Predicate<Edge> pred = e -> edgeIsNotCoveredByTree(tree, e);
+        TreeConstructor c = new TreeConstructor();
+        c.addVertex(centralVertex);
 
         while (edges.size() > 0) {
             Edge edge = edges.iterator().next();
             List<Vertex> vertices = new LinkedList<>(edge.getVertices());
 
-            Map.Entry<Vertex, List<Vertex>> insertionPoint = tree.entrySet().stream()
+            Map.Entry<Vertex, List<Vertex>> insertionPoint = c.tree.entrySet().stream()
                     .filter(it -> vertices.contains(it.getKey())).findFirst().get();
             vertices.remove(insertionPoint.getKey());
 
             Vertex newPoint = vertices.get(0);
             insertionPoint.getValue().add(newPoint);
 
-            tree.put(newPoint, new ArrayList<>());
+            c.addVertex(newPoint);
+            System.out.println("Visit " + newPoint.getName() + " from " + insertionPoint.getKey().getName());
 
             edges.addAll(newPoint.getEdges());
-            edges = new TreeSet<>(edges.stream().filter(pred).collect(Collectors.toList()));
+            edges = new TreeSet<>(edges.stream().filter(c.pred).collect(Collectors.toList()));
         }
 
-        this.tree = tree;
+        this.tree = c.tree;
     }
 
-    private static boolean edgeIsNotCoveredByTree(Map<Vertex, List<Vertex>> tree, Edge edge) {
-        return edge.getVertices().stream().filter(it -> !tree.keySet().contains(it)).count() > 0;
+    // TODO elaborate
+    private static class TreeConstructor {
+        public Map<Vertex, List<Vertex>> tree = new HashMap<>();
+        Predicate<Edge> pred = e -> edgeIsNotCoveredByTree(tree, e);
+
+        public void addVertex(Vertex vertex) {
+            tree.put(vertex, new ArrayList<>());
+        }
+
+        private static boolean edgeIsNotCoveredByTree(Map<Vertex, List<Vertex>> tree, Edge edge) {
+            return edge.getVertices().stream().filter(it -> !tree.keySet().contains(it)).count() > 0;
+        }
     }
 }
