469ca55a07a206b4fe30f5476b5c04d0bdc4a87e
Christian Fuchs <cfuchs@patchcode.ch>
2018-01-27 21:32:34 +0100

    + extracted some util code into a builder class

--- src/main/java/ch/patchcode/port_royale_3/routes/GreedyMinimumDistanceSpanningTree.java
+++ src/main/java/ch/patchcode/port_royale_3/routes/GreedyMinimumDistanceSpanningTree.java
@@ -1,13 +1,7 @@
 package ch.patchcode.port_royale_3.routes;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import ch.patchcode.port_royale_3.routes.DistanceGraph.Edge;
 import ch.patchcode.port_royale_3.routes.DistanceGraph.Vertex;
@@ -27,50 +21,13 @@ public class GreedyMinimumDistanceSpanningTree {
         Vertex centralVertex = graph.getVertices().stream().map(NeighbourDistanceScore::new).sorted().findFirst().get()
                 .getVertex();
 
-        TreeBuilder c = new TreeBuilder(centralVertex);
+        SpanningTreeBuilder builder = new SpanningTreeBuilder(centralVertex);
 
-        while (c.openEdges.size() > 0) {
-            Edge shortestUnconnectedEdge = c.openEdges.iterator().next();
-            List<Vertex> vertices = new LinkedList<>(shortestUnconnectedEdge.getVertices());
-
-            Vertex insertionPoint = c.tree.entrySet().stream()
-                    .filter(it -> vertices.contains(it.getKey())).findFirst().get().getKey();
-            vertices.remove(insertionPoint);
-
-            Vertex newPoint = vertices.get(0);
-            c.addSubVertex(insertionPoint, newPoint);
-
-            System.out.println("Visit " + newPoint.getName() + " from " + insertionPoint.getName());
-
-            c.openEdges.addAll(newPoint.getEdges());
-            c.openEdges = new TreeSet<>(c.openEdges.stream().filter(c.pred).collect(Collectors.toList()));
+        while (builder.hasOpenEdges()) {
+            Edge shortestUnconnectedEdge = builder.shortestOpenEdge();
+            builder.connectEdge(shortestUnconnectedEdge);
         }
 
-        this.tree = c.tree;
-    }
-
-    // TODO elaborate
-    private static class TreeBuilder {
-        public Map<Vertex, List<Vertex>> tree;
-        public TreeSet<Edge> openEdges;
-        Predicate<Edge> pred;
-
-        public TreeBuilder(Vertex centralVertex) {
-            Map<Vertex, List<Vertex>> tree = new HashMap<>();
-            tree.put(centralVertex, new ArrayList<>());
-            TreeSet<Edge> openEdges = new TreeSet<>(centralVertex.getEdges());
-            this.tree = tree;
-            this.openEdges = openEdges;
-            this.pred = e -> edgeIsNotCoveredByTree(tree, e);
-        }
-
-        public void addSubVertex(Vertex insertionPoint, Vertex newPoint) {
-            tree.get(insertionPoint).add(newPoint);
-            tree.put(newPoint, new ArrayList<>());
-        }
-
-        private static boolean edgeIsNotCoveredByTree(Map<Vertex, List<Vertex>> tree, Edge edge) {
-            return edge.getVertices().stream().filter(it -> !tree.keySet().contains(it)).count() > 0;
-        }
+        this.tree = builder.getTree();
     }
 }
---
+++ src/main/java/ch/patchcode/port_royale_3/routes/SpanningTreeBuilder.java
@@ -0,0 +1,63 @@
+package ch.patchcode.port_royale_3.routes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import ch.patchcode.port_royale_3.routes.DistanceGraph.Edge;
+import ch.patchcode.port_royale_3.routes.DistanceGraph.Vertex;
+
+public class SpanningTreeBuilder {
+    public Map<Vertex, List<Vertex>> tree;
+    private TreeSet<Edge> openEdges;
+    private Predicate<Edge> pred;
+
+    public SpanningTreeBuilder(Vertex centralVertex) {
+        Map<Vertex, List<Vertex>> tree = new HashMap<>();
+        tree.put(centralVertex, new ArrayList<>());
+        TreeSet<Edge> openEdges = new TreeSet<>(centralVertex.getEdges());
+        this.tree = tree;
+        this.openEdges = openEdges;
+        this.pred = e -> edgeIsNotCoveredByTree(tree, e);
+    }
+
+    public Map<Vertex, List<Vertex>> getTree() {
+        return tree;
+    }
+
+    public boolean hasOpenEdges() {
+        return openEdges.size() > 0;
+    }
+
+    public Edge shortestOpenEdge() {
+        return openEdges.iterator().next();
+    }
+
+    public void connectEdge(Edge openEdge) {
+        List<Vertex> vertices = new LinkedList<>(openEdge.getVertices());
+        Vertex insertionPoint = tree.entrySet().stream()
+                .filter(it -> vertices.contains(it.getKey())).findFirst().get().getKey();
+        vertices.remove(insertionPoint);
+
+        Vertex newPoint = vertices.get(0);
+        addSubVertex(insertionPoint, newPoint);
+
+        System.out.println("Visit " + newPoint.getName() + " from " + insertionPoint.getName());
+    }
+
+    private void addSubVertex(Vertex insertionPoint, Vertex newPoint) {
+        tree.get(insertionPoint).add(newPoint);
+        tree.put(newPoint, new ArrayList<>());
+        openEdges.addAll(newPoint.getEdges());
+        openEdges = new TreeSet<>(openEdges.stream().filter(pred).collect(Collectors.toList()));
+    }
+
+    private static boolean edgeIsNotCoveredByTree(Map<Vertex, List<Vertex>> tree, Edge edge) {
+        return edge.getVertices().stream().filter(it -> !tree.keySet().contains(it)).count() > 0;
+    }
+}
