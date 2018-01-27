aaa690dff779e00d8e9f1b098713f38aaa8936b9
Christian Fuchs <cfuchs@patchcode.ch>
2018-01-27 10:43:41 +0100

    + fixed naming

--- src/test/java/ch/patchcode/port_royale_3/routes/DistanceCsvDataTest.java
+++ src/test/java/ch/patchcode/port_royale_3/routes/DistanceCsvDataTest.java
@@ -12,27 +12,27 @@ import org.junit.Test;
 
 public class DistanceCsvDataTest {
 
-    private DistanceCsvData reader;
+    private DistanceCsvData data;
 
     @Before
     public void setup() throws IOException {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream("distances_simple.csv")) {
-            reader = new DistanceCsvData(is);
+            data = new DistanceCsvData(is);
         }
     }
 
     @Test
     public void allPlacesArePresent() {
-        assertThat(reader.getPlaces(), containsInAnyOrder("1st Place", "2nd Place", "3rd Place"));
+        assertThat(data.getPlaces(), containsInAnyOrder("1st Place", "2nd Place", "3rd Place"));
     }
 
     @Test
     public void allDistancesAreCorrect() {
-        assertThat(reader.getDistance("1st Place", "2nd Place"), closeTo(1.5, 0.01));
-        assertThat(reader.getDistance("1st Place", "3rd Place"), closeTo(2.0, 0.01));
-        assertThat(reader.getDistance("2nd Place", "1st Place"), closeTo(1.5, 0.01));
-        assertThat(reader.getDistance("2nd Place", "3rd Place"), closeTo(1.2, 0.01));
-        assertThat(reader.getDistance("3rd Place", "1st Place"), closeTo(2.0, 0.01));
-        assertThat(reader.getDistance("3rd Place", "2nd Place"), closeTo(1.2, 0.01));
+        assertThat(data.getDistance("1st Place", "2nd Place"), closeTo(1.5, 0.01));
+        assertThat(data.getDistance("1st Place", "3rd Place"), closeTo(2.0, 0.01));
+        assertThat(data.getDistance("2nd Place", "1st Place"), closeTo(1.5, 0.01));
+        assertThat(data.getDistance("2nd Place", "3rd Place"), closeTo(1.2, 0.01));
+        assertThat(data.getDistance("3rd Place", "1st Place"), closeTo(2.0, 0.01));
+        assertThat(data.getDistance("3rd Place", "2nd Place"), closeTo(1.2, 0.01));
     }
 }
