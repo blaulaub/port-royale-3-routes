099f14ab36ddd9e23308ccffa1545de11cec95c2
Christian Fuchs <cfuchs@patchcode.ch>
2018-01-31 23:39:23 +0100

    + make tour optimizer more encapsulate (hide methods)

--- src/main/java/ch/patchcode/port_royale_3/routes/TourShortcutOptimizer.java
+++ src/main/java/ch/patchcode/port_royale_3/routes/TourShortcutOptimizer.java
@@ -32,67 +32,6 @@ public class TourShortcutOptimizer {
         this.links = links;
     }
 
-    public List<Vertex> computeRedundantVertices() {
-        return links.entrySet().stream().filter(it -> it.getValue().size() / 2 > 1)
-                .map(it -> it.getKey()).collect(Collectors.toList());
-    }
-
-    public List<ShortcutMetric> computeAllShortcutMetrics(List<Vertex> redundantVertices) {
-        List<ShortcutMetric> metrics = new ArrayList<>();
-        for (Vertex v : redundantVertices) {
-
-            Set<Vertex> neighbours = links.get(v).stream().distinct().collect(Collectors.toSet());
-
-            List<Edge> edges = v.getEdges().stream()
-                    .filter(it -> it.getVertices().stream().anyMatch(it2 -> neighbours.contains(it2)))
-                    .collect(Collectors.toList());
-
-            for (int i = 0; i < edges.size(); ++i) {
-                Edge edge1 = edges.get(i);
-                Vertex v1 = edge1.getVertices().stream().filter(it -> !it.equals(v)).findFirst().get();
-                for (int j = i + 1; j < edges.size(); ++j) {
-                    Edge edge2 = edges.get(j);
-                    Vertex v2 = edge2.getVertices().stream().filter(it -> !it.equals(v)).findFirst().get();
-                    Edge edge3 = v1.getEdges().stream().filter(it -> it.getVertices().contains(v2)).findFirst().get();
-
-                    double benefit = Math.max(0, edge1.getWeight()+edge2.getWeight()-edge3.getWeight());
-                    metrics.add(new ShortcutMetric(v, Arrays.asList(v1, v2), -benefit));
-                }
-            }
-        }
-        return metrics;
-    }
-
-    public void applyShortcut(ShortcutMetric top) {
-        links.get(top.center).remove(top.neighbours.get(0));
-        links.get(top.center).remove(top.neighbours.get(1));
-        links.get(top.neighbours.get(0)).remove(top.center);
-        links.get(top.neighbours.get(1)).remove(top.center);
-        links.get(top.neighbours.get(0)).add(top.neighbours.get(1));
-        links.get(top.neighbours.get(1)).add(top.neighbours.get(0));
-    }
-
-    public boolean isUnsplitted() {
-        Set<Vertex> colored = new HashSet<Vertex>();
-        Deque<Vertex> candidates = new ArrayDeque<>();
-        candidates.add(links.keySet().iterator().next());
-        while (candidates.size() > 0) {
-            Vertex v = candidates.pop();
-            colored.add(v);
-            links.get(v).stream().distinct().filter(it -> !colored.contains(it)).forEach(it -> candidates.add(it));
-        }
-        return colored.size() == links.size();
-    }
-
-    public void unapplyShortcut(ShortcutMetric top) {
-        links.get(top.center).add(top.neighbours.get(0));
-        links.get(top.center).add(top.neighbours.get(1));
-        links.get(top.neighbours.get(0)).add(top.center);
-        links.get(top.neighbours.get(1)).add(top.center);
-        links.get(top.neighbours.get(0)).remove(top.neighbours.get(1));
-        links.get(top.neighbours.get(1)).remove(top.neighbours.get(0));
-    }
-
     public List<Vertex> createTour() {
         List<Vertex> redundantVertices = computeRedundantVertices();
         while (redundantVertices.size() > 0) {
@@ -125,5 +64,66 @@ public class TourShortcutOptimizer {
             node = links.get(node.get()).stream().filter(it -> !visited.contains(it)).findFirst();
         }
         return visited;
+    }
+
+    private List<Vertex> computeRedundantVertices() {
+        return links.entrySet().stream().filter(it -> it.getValue().size() / 2 > 1)
+                .map(it -> it.getKey()).collect(Collectors.toList());
+    }
+
+    private List<ShortcutMetric> computeAllShortcutMetrics(List<Vertex> redundantVertices) {
+        List<ShortcutMetric> metrics = new ArrayList<>();
+        for (Vertex v : redundantVertices) {
+
+            Set<Vertex> neighbours = links.get(v).stream().distinct().collect(Collectors.toSet());
+
+            List<Edge> edges = v.getEdges().stream()
+                    .filter(it -> it.getVertices().stream().anyMatch(it2 -> neighbours.contains(it2)))
+                    .collect(Collectors.toList());
+
+            for (int i = 0; i < edges.size(); ++i) {
+                Edge edge1 = edges.get(i);
+                Vertex v1 = edge1.getVertices().stream().filter(it -> !it.equals(v)).findFirst().get();
+                for (int j = i + 1; j < edges.size(); ++j) {
+                    Edge edge2 = edges.get(j);
+                    Vertex v2 = edge2.getVertices().stream().filter(it -> !it.equals(v)).findFirst().get();
+                    Edge edge3 = v1.getEdges().stream().filter(it -> it.getVertices().contains(v2)).findFirst().get();
+
+                    double benefit = Math.max(0, edge1.getWeight()+edge2.getWeight()-edge3.getWeight());
+                    metrics.add(new ShortcutMetric(v, Arrays.asList(v1, v2), -benefit));
+                }
+            }
+        }
+        return metrics;
+    }
+
+    private void applyShortcut(ShortcutMetric top) {
+        links.get(top.center).remove(top.neighbours.get(0));
+        links.get(top.center).remove(top.neighbours.get(1));
+        links.get(top.neighbours.get(0)).remove(top.center);
+        links.get(top.neighbours.get(1)).remove(top.center);
+        links.get(top.neighbours.get(0)).add(top.neighbours.get(1));
+        links.get(top.neighbours.get(1)).add(top.neighbours.get(0));
+    }
+
+    private boolean isUnsplitted() {
+        Set<Vertex> colored = new HashSet<Vertex>();
+        Deque<Vertex> candidates = new ArrayDeque<>();
+        candidates.add(links.keySet().iterator().next());
+        while (candidates.size() > 0) {
+            Vertex v = candidates.pop();
+            colored.add(v);
+            links.get(v).stream().distinct().filter(it -> !colored.contains(it)).forEach(it -> candidates.add(it));
+        }
+        return colored.size() == links.size();
+    }
+
+    private void unapplyShortcut(ShortcutMetric top) {
+        links.get(top.center).add(top.neighbours.get(0));
+        links.get(top.center).add(top.neighbours.get(1));
+        links.get(top.neighbours.get(0)).add(top.center);
+        links.get(top.neighbours.get(1)).add(top.center);
+        links.get(top.neighbours.get(0)).remove(top.neighbours.get(1));
+        links.get(top.neighbours.get(1)).remove(top.neighbours.get(0));
     }
 }
